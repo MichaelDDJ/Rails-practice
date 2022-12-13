8fab77c4e2959b7fc32b9fa3426876ca551f9414
Michael Davis <jrmdd1496@gmail.com>
2022-12-12 21:04:20 -0600

    Whatever changes.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Pet.create([{
+    name: "Timmy",
+    breed: "Siamese"
+},
+{
+    name: "King",
+    breed: "Tabby"
+},
+{
+    name: "Lucy",
+    breed: "Siberian"
+}])
